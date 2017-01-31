a65373d8b04739dd76212ea9b4ca86f97cdc82f9
jsonlitz <jasondotlee@gmail.com>
2017-01-31 00:46:08 -0800

    create reddit object and practice pure functions

--- app/index.js
+++ app/index.js
@@ -1,15 +1,61 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
 
+var REDDIT_DATA = {
+  name: 'reddit',
+  subreddit: 'aww',
+  image: 'https://i.imgur.com/VaeBUuj.gifv'
+}
+
+
+var awwImage: React.createClass({
+  render: function() {
+    return <img src= {this.props.imageUrl} style = {{heigh: 100, width: 100}}/>
+  }
+});
+
+var redditLink: React.createClass({
+  render: function(){
+    return (
+      <div>
+        <a href={https:'//www.reddit.com/r/' + this.props.subreddit}>
+          { }
+        </a>
+      </div>
+    )
+  }
+});
+
+var reddit: React.createClass({
+  render: function() {
+    return (
+      <div>
+        {this.props.name}
+      </div>
+    )
+  }
+});
+
+var r_aww: React.createClass({
+  render: function() {
+    return (
+      <div>
+        <awwImage imageUrl= {this.props.reddit.image}
+      </div>
+    )
+  }
+})
+
+
 var HelloWorld = React.createClass({
   render: function(){
     return (
-      <div> Welcome to the National Athletics League </div>
+      <div> Welcome to the {this.props.pageName} ! </div>
     )
   }
 });
 
 ReactDOM.render(
-  <HelloWorld />,
+  <HelloWorld pageName = "The National Athletics League" />,
   document.getElementById('app')
 )
